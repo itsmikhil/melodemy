d39305e27ad288002340bdc5db5a5704a991af21
mikhil <ailani.mikhil@gmail.com>
2025-06-10 23:01:08 +0530

    completed hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import HeroSection from "@/components/HeroSection";
 import NavBar from "@/components/NavBar";
 
 
 export default function Home() {
   return (
-    <div className="w-screen min-h-screen bg-black text-white">
+    <div className="max-w-screen min-h-screen bg-black text-white">
       <NavBar/>
+      <HeroSection/>
     </div>
   );
 }
